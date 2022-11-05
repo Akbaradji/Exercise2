2f29aa1590061e9478760dbe3704316aba975104
Akbar Adji Herlambang <akbaradjiherlambang@gmail.com>
2022-11-04 16:14:15 -1200

    create class program

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    class program
+    {
+        int[] arr = new int[0];
+        int n;
+        int i;
+    }
+}
