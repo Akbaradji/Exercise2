a10683b77ef525cbdbcf6e550e634fa1297fe0b0
Akbar Adji Herlambang <akbaradjiherlambang@gmail.com>
2022-11-04 16:20:45 -1200

    memasukkan element minimum 1 dan maximum 41

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -8,8 +8,22 @@ namespace Exercise2
 {
     class program
     {
-        int[] arr = new int[0];
+        int[] arr = new int[41];
         int n;
         int i;
+
+        public void input()
+        {
+            while(true)
+            {
+                Console.WriteLine("Enter the number of elements in the array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <=41))
+                    break;
+                else
+                    Console.WriteLine("\n Array should have minimum 1 and maximum 41 elements.\n");
+            }
+        }
     }
 }
